5a080556b1285004522b8d3b4b6834ddf1c87eae
Cristina McLaughlin <cemclaug@hawaii.edu>
2018-02-06 11:24:14 -0900

    fiberoo

---
+++ fiberoo.js
@@ -0,0 +1,19 @@
+function fib() {
+  let x = [0, 1];
+
+  for (let i = 2; i < 100; i++){
+    x[i] = x[i - 1] + x[i - 2];
+  }
+  return x;
+}
+
+
+function numsToStrings(array) {
+  return _.map(array, function (num) { return `${num}`; });
+}
+
+console.log(numsToStrings(fib()));
+
+functionEvenNums(array){
+    return_.reduce(array, function)
+}
